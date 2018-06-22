6d13f3f7242ae78d143c3d83fa8b5f233cd25a8a
Anatoly Yusufov <anatoly.yusufov@gmail.com>
2018-06-22 16:53:07 -0400

    Session page Encapsulated

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,4 @@
 require('dotenv').config();
 module.exports = {
-    url: 'mongodb+srv://' + process.env.DB_USER + ':' +  process.env.DB_PASSWORD + '@cluster0-iffqw.mongodb.net/test?retryWrites=true'
+    url: 'mongodb+srv://' + process.env.DB_USER + ':' +  process.env.DB_PASSWORD + '@cluster0-iffqw.mongodb.net/' + process.env.DB || 'analytics' + '?retryWrites=true'
 }
